75b1360866429b44467eccfa4aef35ff9a1d5719
Rui Campos <ruifilipedesousacampos@gmail.com>
2023-07-14 07:43:27 +0000

    Delete everything

--- typedAPI/__init__.py
+++
@@ -1,4 +0,0 @@
-from .resource_path.schema import ResourcePath
-from .headers.schema import Headers
-from .server import Server
-from .body.schema import MultiPartFormData
--- typedAPI/endpoint/data.py
+++
@@ -1,7 +0,0 @@
-
-import typing
-import typedAPI.endpoint.schema
-
-
-http_methods = typing.get_args(typedAPI.endpoint.schema.HttpMethods)
-
